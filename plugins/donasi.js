fb086fd46a3a8801cd29a6b99a095a5ce35bb7a4
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-03-15 13:05:57 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,13 +1,8 @@
-let { MessageType } = require('@adiwajshing/baileys')
-let fs = require ('fs')
-let handler = async (m, { conn }) => {
-	
-
-let { name, limit, level, role, age, money, healt, premium, registered } = global.DATABASE.data.users[m.sender] 
-pp = await conn.getProfilePicture(global.conn.user.jid)
-conn.sendButtonImg(m.chat, '┏━━⬣ 𝗗𝗢𝗡𝗔𝗦𝗜\n┃\n┃⬡ Maxis : 60147366955\n┃⬡ GOPAY : 62895612153565', `${pp}`, `Created By Putbotz`.trim(), 'MENU', '#allmenu', { quoted: m })
-
-  }
+let handler = async m => m.reply(`
+┌「 *Topap • Maxis* 」
+├ 60147366955
+└────
+`.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
